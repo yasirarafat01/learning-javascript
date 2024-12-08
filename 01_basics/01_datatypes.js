00a3208d1761e4236a2bd72efb1a2bfbec30e1b4
yasirarafat01 <164495925+yasirarafat01@users.noreply.github.com>
2024-12-08 15:55:50 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";// treat all js code as a newer version
+// alert(5+6); // alert is a function that displays a message in a pop-up window
+
+// data types
+// number: 0 to 9
+// string: "Name"
+// boolean: True and False
+// BigInt:
+// null : stand alone value
+// undefined: variable is declared but not assigned a value
+// object: collection of key-value pairs
+// symbol: unique and immutable primitive values
+// array: collection of values in a specific order
+// function: block of code that can be executed multiple times
--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,3 +5,8 @@ accountCity = "allahabad";
 console.table([accountId, accountName, accountPassword, accountCity]);
 // const myname = "yasir arafat";
 // console.log(myname);
+
+/* 
+prefer not to use var keyword  becousse of an issue block scope and
+functional  scope
+*/
