937699438378f3bdb2b52ca24a804759ecb4d8aa
yasirarafat01 <164495925+yasirarafat01@users.noreply.github.com>
2024-12-08 09:31:25 +0000

    variables complete

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345678;
+let accountName = "John Doe";
+var accountPassword = "123456";
+accountCity = "allahabad";
+console.table([accountId, accountName, accountPassword, accountCity]);
+// const myname = "yasir arafat";
+// console.log(myname);
