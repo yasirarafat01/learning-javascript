0aa288fa2f1616146b1a3321ddc85b3329cb9e27
yasirarafat01 <164495925+yasirarafat01@users.noreply.github.com>
2024-12-09 11:12:39 +0000

    coversion of datatypes

---
+++ 01_basics/03_ConversionOpration.js
@@ -0,0 +1,18 @@
+let score = "78hgfh";
+console.log(typeof score); // Output: string
+
+let valueInNumber = Number (score);
+console.log(typeof valueInNumber); // Output: number
+console.log(valueInNumber); 
+
+// 33 => 33
+// string => NaN
+// True => 1 ; False => 0
+// null => 0
+// undefined => NaN
+
+let SomeNumber = 45
+let StrinNumber = String (SomeNumber);
+console.log(StrinNumber);
+console.log(typeof StrinNumber);
+
